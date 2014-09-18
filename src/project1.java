7b1aa1be6ac879c38c6f3df39151886992ffd63c
Marist User <Joe.Gutierrez1@marist.edu>
2014-09-18 18:15:16 -0400

    first attempt at a commit

---
+++ src/project1.java
@@ -0,0 +1,84 @@
+/**
+ * Created by Marist User on 9/18/2014.
+ */
+public class project1 {
+}
+
+
+<html>
+<head>
+<h1>Lawn Game Olympics</h1>
+<h3>Labor Day 2014</h3>
+<br>
+</head>
+
+<body>
+
+<p>
+<b>Olympiads</b>
+</p>
+
+<hr>
+<br>
+<u>Male Competitors:</u>
+<br>
+<br>
+
+<script type="text/javascript">
+
+
+        var names1 =new Array("Joe","Dylan","Matthew","Michael","Joshua","James","Edward","Milo");
+
+        document.writeln(names1[0]);
+        document.writeln("<br>" + names1[1]);
+        document.writeln("<br>" + names1[2]);
+        document.writeln("<br>" + names1[3]);
+        document.writeln("<br>" + names1[4]);
+        document.writeln("<br>" + names1[5]);
+        document.writeln("<br>" + names1[6]);
+        document.writeln("<br>" + names1[7]);
+        document.writeln("<br>");
+</script>
+
+<br>
+<u>Female Competitors:</u>
+<br>
+<br>
+
+
+<script type="text/javascript">
+
+        var names2 =new Array("Elizabeth","Ann","Alyssa","Corrin","Nicole","Natalie","Cynthia","Sylvia");
+
+        document.writeln(names2[0]);
+        document.writeln("<br>" + names2[1]);
+        document.writeln("<br>" + names2[2]);
+        document.writeln("<br>" + names2[3]);
+        document.writeln("<br>" + names2[4]);
+        document.writeln("<br>" + names2[5]);
+        document.writeln("<br>" + names2[6]);
+        document.writeln("<br>" + names2[7]);
+        document.writeln("<br>");
+</script>
+
+<br>
+<u>Events:</u>
+<br>
+<br>
+
+<script type="text/javascript">
+
+        var events =new Array("Washoos","Can Jam","Horseshoes","Cornhole","Ladderball","Stickgame");
+
+        document.writeln(events[0]);
+        document.writeln("<br>" + events[1]);
+        document.writeln("<br>" + events[2]);
+        document.writeln("<br>" + events[3]);
+        document.writeln("<br>" + events[4]);
+        document.writeln("<br>" + events[5]);
+        document.writeln("<br>");
+
+</script>
+
+</body>
+</html>
